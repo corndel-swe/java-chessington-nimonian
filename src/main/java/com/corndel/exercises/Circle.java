8439bf68dc9bb90425cfeecbaf3d7eb3de52a875
Simon Haines <nimon@hotmail.co.uk>
2024-09-22 20:34:10 +0100

    solves day 3

--- src/main/java/com/corndel/exercises/Circle.java
+++ src/main/java/com/corndel/exercises/Circle.java
@@ -7,15 +7,36 @@ public class Circle implements Shape {
     this.radius = radius;
   }
 
+  /**
+   * Returns the radius of this Circle.
+   *
+   * @return the radius
+   */
   public double getRadius() {
     return radius;
   }
 
+  /**
+   * Returns the perimeter of this Circle.
+   *
+   * The perimeter is defined as <i>2 * PI * r</i>, where <i>r</i> is the radius
+   * of the circle.
+   *
+   * @return the perimeter
+   */
   @Override
   public double getPerimeter() {
     return 2 * Maths.PI * this.getRadius();
   }
 
+  /**
+   * Returns the area of this Circle.
+   *
+   * The area is defined as <i>PI * r^2</i>, where <i>r</i> is the radius of
+   * the circle.
+   *
+   * @return the area
+   */
   @Override
   public double getArea() {
     return Maths.PI * this.getRadius() * this.getRadius();
--- src/main/java/com/corndel/exercises/Counter.java
+++ src/main/java/com/corndel/exercises/Counter.java
@@ -7,14 +7,25 @@ public class Counter {
     this.count = 0;
   }
 
+  /**
+   * Increment the count by one.
+   */
   public void increment() {
     this.count++;
   }
 
+  /**
+   * Reset the count to zero.
+   */
   public void reset() {
     this.count = 0;
   }
 
+  /**
+   * Get the current count.
+   *
+   * @return The current count.
+   */
   public int getCount() {
     return this.count;
   }
